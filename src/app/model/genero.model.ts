f5f9bb21b0432ee8178a26d9584ae506d940aee8
juliojose3000 <juliojose3000@gmail.com>
2018-05-19 14:33:28 -0600

    limpieza total

--- src/app/model/genero.model.ts
+++
@@ -1,8 +0,0 @@
-export class Genero{
-    codGenero:number;
-    nombreGenero:string;
-    constructor(codGenero?:number,nombreGenero?:string){
-        this.codGenero=codGenero;
-        this.nombreGenero=nombreGenero;
-    }
-}
