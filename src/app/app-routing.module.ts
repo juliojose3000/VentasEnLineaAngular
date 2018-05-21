b6a318e567ed2c1cef189aae6a499718ec18abb5
Allan Josué Solano Salazar <ajosue19@gmail.com>
2018-05-21 00:04:53 -0600

    Ultima 21-05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import {RouterModule,Routes} from '@angular/router'
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
 import {AdministrativoComponent} from './administrativo/administrativo.component'
+import {LogRegClienteComponent} from './log-reg-cliente/log-reg-cliente.component'
+import {LogAdminComponent} from  './log-admin/log-admin.component'
+import {MantenimientoProductoComponent} from './mantenimiento-producto/mantenimiento-producto.component'
 const router:Routes=[
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:HomeComponent},
     {path:'administrativo',component:AdministrativoComponent},
-    {path:'about', component:AboutComponent}
+    {path:'logRegistroCliente',component:LogRegClienteComponent},
+    {path:'about', component:AboutComponent},
+    {path:'logAdmin', component:LogAdminComponent},
+    {path:'mantenimientoProducto',component:MantenimientoProductoComponent}
     ];//arreglo Routes
     
     @NgModule({//decoracion
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,19 @@ import { HomeComponent } from './home/home.component';
 
 import {AppRoutingModule} from './app-routing.module';
 import { AdministrativoComponent } from './administrativo/administrativo.component';
+import { LogRegClienteComponent } from './log-reg-cliente/log-reg-cliente.component';
+import { LogAdminComponent } from './log-admin/log-admin.component';
+import { MantenimientoProductoComponent } from './mantenimiento-producto/mantenimiento-producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     HomeComponent,
-    AdministrativoComponent
+    AdministrativoComponent,
+    LogRegClienteComponent,
+    LogAdminComponent,
+    MantenimientoProductoComponent
   ],
   imports: [
     BrowserModule,
